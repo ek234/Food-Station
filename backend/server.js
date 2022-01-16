6051803037ec0dbd22d6be70629780c2e4aa387b
ek234 <71319388+ek234@users.noreply.github.com>
2022-01-16 22:32:25 +0530

    backend: added server.js

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+const MONGO_PORT = 27017;
+const DB_NAME = "foodorderer"
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:' + MONGO_PORT + '/' + DB_NAME, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+	console.log("MongoDB connected");
+})
+
+/////////////////////////////////////////////////////////////////////////todo: add routes and API endpoints
+// routes
+var CustRouter = require("./routes/api/cust")
+// setup API endpoints
+app.use("/api/cust", CustRouter);
+
+app.listen(PORT, function() {
+	console.log("server is running on port: " + PORT);
+});
