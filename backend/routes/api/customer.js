60689bf89a54099307cad81fe83e8a24c07b9f1f
ek234 <71319388+ek234@users.noreply.github.com>
2022-01-17 13:35:05 +0530

    backend: api: added if-else cascade to avoid async returns

--- backend/routes/api/customer.js
+++ backend/routes/api/customer.js
@@ -10,63 +10,56 @@ const validateLoginInput = require("../../validation/loginCust");
 const Cust = require("../../models/customers");
 
 router.post("/register", (req, res) => {
-
-//console.log("entered");
-
-		var validation = validateRegisterInput(req.body);
-		if (!validation.isValid) {
-			return res.status(400).json(validation.errors);
-		}
-//console.log("validated input");
-		Cust.findOne({ email: req.body.email }).then(cust => {
-			if (cust) {
-				return res.status(400).json({ email: "email already registered" });
-			} else {
-				const newCust = new Cust({
-					name: req.body.name,
-					email: req.body.email,
-					contact: req.body.contact,
-					age: req.body.age,
-					batch: req.body.batch,
-					password: req.body.password
-				});
-				newCust.save().then(cust => res.json(cust)).catch(err => console.log(err));
-			}
-		});
+	var validation = validateRegisterInput(req.body);
+	if (validation.isValid) {
+		Cust.findOne({ email: req.body.email }).then(cust => { if (cust) {
+			const newCust = new Cust({
+				name: req.body.name,
+				email: req.body.email,
+				contact: req.body.contact,
+				age: req.body.age,
+				batch: req.body.batch,
+				password: req.body.password
+			});
+			newCust.save().then(cust => res.json(cust)).catch(err => console.log(err));
+		} else {
+			return res.status(400).json({ email: "email already registered" });
+		}});
+	} else {
+		return res.status(400).json(validation.errors);
+	}
 });
 
 router.post("/login", (req, res) => {
-
-		var validation = validateLoginInput(req.body);
-		if (!validation.isValid) {
-			return res.status(400).json(validation.errors);
-		}
-		Cust.findOne({ email: req.body.email }).then(cust => {
-			if (!cust) {
-				return res.status(400).json({ email: "email not registered" });
+	var validation = validateLoginInput(req.body);
+	if (validation.isValid) {
+		Cust.findOne({ email: req.body.email }).then(cust => { if (cust) {
+			if (cust.password === req.body.password) {
+				const payload = {
+					id: cust.email
+				};
+				jwt.sign(
+					payload,
+					Key,
+					{
+						expiresIn: 86400
+					},
+					(err, token) => {
+						res.json({
+							success: true,
+							token: "Bearer " + token
+						});
+					}
+				);
 			} else {
-				if (cust.password === req.body.password) {
-					const payload = {
-						id: cust.email
-					};
-					jwt.sign(
-						payload,
-						Key,
-						{
-							expiresIn: 86400
-						},
-						(err, token) => {
-							res.json({
-								success: true,
-								token: "Bearer " + token
-							});
-						}
-					);
-				} else {
-					return res.status(400).json({ passwordwrong: "wrong password" });
-				}
+				return res.status(400).json({ passwordwrong: "wrong password" });
 			}
-		});
+		} else {
+			return res.status(400).json({ email: "email not registered" });
+		}});
+	} else {
+		return res.status(400).json(validation.errors);
+	}
 });
 
 module.exports = router;
