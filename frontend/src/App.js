4f016778c26de2e2efc118d25fb3d344fbd3e5f0
ek234 <71319388+ek234@users.noreply.github.com>
2022-01-23 17:57:46 +0530

    frontend: added base scripts

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import "./App.css";
+
+import VendsList from "./components/users/VendsList";
+import CustsList from "./components/users/CustsList";
+import Home from "./components/common/Home";
+import Register from "./components/common/Register";
+import Navbar from "./components/templates/Navbar";
+import Profile from "./components/users/Profile";
+
+const Layout = () => {
+	return (
+		<div>
+		<Navbar />
+		<div className="container">
+		<Outlet />
+		</div>
+		</div>
+	);
+};
+
+function App() {
+	return (
+		<BrowserRouter>
+		<Routes>
+		<Route path="/" element={<Layout />}>
+		<Route path="/" element={<Home />} />
+		<Route path="custs" element={<CustsList />} />
+		<Route path="vends" element={<VendsList />} />
+		<Route path="register" element={<Register />} />
+		<Route path="profile" element={<Profile />} />
+		</Route>
+		</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
