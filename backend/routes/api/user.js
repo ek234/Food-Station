afcea6cfb15a51d0e7927f64128cd23128acd984
ek234 <71319388+ek234@users.noreply.github.com>
2022-01-29 23:46:49 +0530

    removed useless import stmt

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const Key = require("../../../secret");
-
 const validateRegisterInputC = require("../../validation/registerCust");
 const validateLoginInputC = require("../../validation/loginCust");
 const validateRegisterInputV = require("../../validation/registerVend");
